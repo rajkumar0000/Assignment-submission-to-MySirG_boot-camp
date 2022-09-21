080acc497f5254d77a4908d2354794a8009ced27
Raj <111216394+rajkumar0000@users.noreply.github.com>
2022-09-22 00:20:38 +0530

    Delete assignment 1.11.c

--- assignment 1.11.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-{
-    int hh,mm;
-    printf("Enter the time in HH:MM \n");
-    scanf("%d:%d",&hh,&mm);     //11 Hour and 25 Minute
-    printf("%d Hour and %d Minuts",hh,mm);
-}
